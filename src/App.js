c26cbe6a0ed9943cc926edea6aa13ff31c51322c
Jordan Cotter <jrdn.cttr@gmail.com>
2021-02-18 20:57:53 -0600

    feat: add quotes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { RandomQuote } from './components'
 import './App.css'
 import githubIcon from './images/github.png'
 import nodeIcon from './images/node_logo.png'
@@ -14,16 +15,6 @@ export const A = styled.a`
     color: blue;
   }
 `
-const P = styled.p``
-const Education = styled.table`
-  td {
-    padding: 10px 10px 10px 0;
-  }
-`
-const SubHeader = styled.div`
-  display: flex;
-  justify-content: space-around;
-`
 const Page = styled.div`
   font-size: calc(10px + 1.3vmin);
   color: white;
@@ -31,43 +22,11 @@ const Page = styled.div`
   margin: auto;
   padding: 50px 0 200px 0;
 `
-const Name = styled.span`
-  font-size: 1.5em;
-`
-const Header = styled.div`
-  display: flex;
-  justify-content: center;
-`
-const SectionHeading = styled.div`
-  font-weight: bold;
-  padding: 20px 0 10px 0;
-`
-const Table = styled.table`
-  border-collapse: collapse;
-  width: 100%;
-  td {
-    border: 1px solid white;
-  }
-`
-const JobHeading = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 50px;
-  span {
-    font-weight: bold;
-    font-style: italic;
-  }
-`
-const ProjectHead = styled.div`
-  padding: 15px 0;
-`
-const GithubIcon = styled.a`
-  transform: translateY(10px);
-`
 
 function App() {
   return (
     <Page>
+      <RandomQuote />
       <A href="/resume">Resume</A><br />
       <A href="https://cant-stop.vercel.app/">Can't Stop Game</A><br />
       <A href="/w2v1099">Taxes Calculator</A><br />
---
+++ src/components/RandomQuote/index.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import quotes from './quotes.js'
+
+export function RandomQuote(props) {
+  return (
+    <div style={{ paddingBottom: 15, textAlign: `center` }}>
+      {getRandomQuote()}
+    </div>
+  );
+
+  function getRandomQuote () {
+    return quotes[Math.floor(Math.random()*quotes.length)]
+  }
+}
+
+RandomQuote.propTypes = {}
+
+export default RandomQuote;
